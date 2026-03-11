45416ad00fa6baf8a52abd735225ba27a00cee5c
BsauerSchool <bsauer@byupathway.edu>
2026-03-11 15:48:44 -0600

    Completed program 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,33 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {  
+// Random number generator
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,99);
+        
+        int numberOfGuesses = 0;
+        string stringGuess;
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            stringGuess = Console.ReadLine();
+            guess = int.Parse(stringGuess);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            numberOfGuesses += 1;
+        } while (guess != number);
+        Console.WriteLine("You Guessed it!!");
+        Console.WriteLine($"Total number of guesses: {numberOfGuesses}");
+
     }
 }
