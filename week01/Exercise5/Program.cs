e5ba1e9fc5944a8e1fe141f04848f8190f8a8a4a
BsauerSchool <bsauer@byupathway.edu>
2026-03-12 15:25:43 -0600

    Completed exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,58 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+// DisplayWelcome - Displays the message, "Welcome to the Program!"
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+// PromptUserName - Asks for and returns the user's name (as a string)
+        string name;
+
+        Console.Write("Please enter your name: ");
+        name = Console.ReadLine();
+
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+// PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+        Console.Write("Enter your favorite number ");
+        string number = Console.ReadLine();
+        
+        int num = int.Parse(number);
+        return num;
+    }
+
+    static int SqaureNumber(int number)
+// SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+    {
+        int num = (int)Math.Pow(number, 2);
+        return num;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+// DisplayResult - Accepts the user's name and the squared number and displays them.
+        Console.WriteLine($"{name}, the square of your number is {number}");
+    }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squaredNum = SqaureNumber(number);
+        DisplayResult(name, squaredNum);
     }
 }
+
+
+// Welcome to the program!
+// Please enter your name: Brother Burton
+// Please enter your favorite number: 42
+// Brother Burton, the square of your number is 1764
