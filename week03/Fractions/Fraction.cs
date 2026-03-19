43a5aefa623bd85baf98d704b885c85a1bd280f7
BsauerSchool <bsauer@byupathway.edu>
2026-03-19 09:23:53 -0600

    Completed Fraction activity

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,57 @@
+using System;
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public int GetTop()
+    {
+        return _numerator;   
+    }
+
+    public int GetBottom()
+    {
+        return _denominator;
+    }
+
+    public void SetTop(int numerator)
+    {
+        _numerator = numerator;
+    }
+
+    public void SetBottom(int denominator)
+    {
+        _denominator = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        return _numerator + "/" + _denominator;
+    }
+
+    public double GetDecimalValue()
+    {
+        double numerator = _numerator;
+        double denominator = _denominator;
+        return numerator/denominator;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction one = new Fraction();
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+
+        Fraction two = new Fraction(2);
+        Console.WriteLine(two.GetFractionString());
+        Console.WriteLine(two.GetDecimalValue());
+
+        Fraction three = new Fraction(4, 3);
+        Console.WriteLine(three.GetFractionString());
+        Console.WriteLine(three.GetDecimalValue());
     }
 }
