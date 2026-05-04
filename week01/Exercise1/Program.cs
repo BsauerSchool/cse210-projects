0603916a73135a1a181b05e1aa7fbc18801f301c
BsauerSchool <bsauer@byupathway.edu>
2026-05-04 14:40:36 -0700

    Exercise 1 Complete

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        // Prompt User for their first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt User for their last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine();
+
+        // Display the text back all on one line (Bond, james bond)
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
