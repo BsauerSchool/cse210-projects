db731e3a370366ee2354566051cc05ad8f3a8913
BsauerSchool <bsauer@byupathway.edu>
2026-05-05 09:43:05 -0700

    Core Requirement 1 complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        // Core Requirement 1: Ask the user for their grade percentage, then write a series of if, else if, else statements to print out the appropriate letter grade.
+
+        // Ask User for their grade percentage
+        Console.WriteLine("What is your Grade Percentage? ");
+        string stringPercentage = Console.ReadLine();
+        int intPercentage = int.Parse(stringPercentage);
+
+        // If A
+        if (intPercentage >= 90)
+        {
+            Console.WriteLine("You Got an A");
+        }
+        else if (intPercentage >= 80)
+        {
+        // If B
+            Console.WriteLine("You got a B");
+        }
+        else if (intPercentage >= 70)
+        {
+        // If C
+            Console.WriteLine("You got a C");
+        }
+        else if (intPercentage >= 60)
+        {
+        // If D
+            Console.WriteLine("You got a D");
+        }
+        else
+        {
+        // If F
+            Console.WriteLine("You got an F");
+        }
+
+
+
+
+        // Core Requirement 2: Assume that you must have at lease a 70 to pass the class. After determiing the letter grade and printing it out, add a separate if statement to determine if the user passed the course, and if so, display a message to congratulate them. If not, display a different message to encourage them for next time.
+
+        // Core Requirement 3: Change your code from the first part, sot that instead of printing the letter grade in the body of each If, else if, else block, instead create a new variable called letter and then in each blcok, set this variable to the appropriate value. Finally,after the whole series of if ,statements, have a  single print statement that prints the letter grade once.
     }
 }
