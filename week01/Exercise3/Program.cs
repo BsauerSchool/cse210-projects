287de0f1a781d43b4b273a8dc166f13099503b44
BsauerSchool <bsauer@byupathway.edu>
2026-05-06 11:42:55 -0700

    Completed Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,41 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string guessString;
+        int guess;
+
+        // Core Requirement 1: Ask the user for the magic number
+        // Core Requirement 3: Random number instead of asking for a magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+        Console.WriteLine(magicNumber);
+        
+
+        // Core Requirement 2: Add a loop that keeps going until the magic number is guessed.
+        do
+        {
+        // Ask the user for a guess
+            Console.Write("What is your guess? "); 
+            guessString = Console.ReadLine();
+            guess = int.Parse(guessString);
+
+            // using an if statement ,determine higher, lower, or you guessed it.
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (guess != magicNumber);
+
+
+
     }
 }
